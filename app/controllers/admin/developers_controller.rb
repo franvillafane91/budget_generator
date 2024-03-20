2264b5f0b28a77703202d33ff4a9b414315c374f
fran <villafanef57@gmal.com>
2024-03-19 23:28:24 -0300

    doc_generator

--- app/controllers/admin/developers_controller.rb
+++ app/controllers/admin/developers_controller.rb
@@ -4,20 +4,6 @@ module Admin
       @developers = Developer.all
     end
 
-    def new
-      @developer = Developer.new
-    end
-
-    def create
-      @developer = Developer.new(dev_params)
-
-      if @developer.update(dev_params)
-        redirect_to admin_developers_path, notice: 'Desarrollador creado.'
-      else
-        redirect_to new_admin_developer_path, notice: 'Hubo un error.'
-      end
-    end
-
     def edit
       @developer = Developer.find(params[:id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get 'welcome/index'
     resources :budgets
     resources :budget_pdf, only: [:show]
-    resources :developers
+    resources :developers, only: [:index, :edit, :update, :destroy]
   end
 
   root 'admin/welcome#index'
